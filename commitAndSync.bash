c36dfc8fbac6256bb234e3929b8ecf818fa9f393
arrowse <autumn@autumn.onl>
2025-01-13 17:31:59 -0600

    Made a bash commit assistance tool

---
+++ commitAndSync.bash
@@ -0,0 +1,14 @@
+echo -e "\e[1;45m Welcome to the Froge commit tool.\e[0m"
+read -r -p "Would you like to automatically commit your changes? [y/N] " response
+if [[ "$response" =~ [yY](es)* ]]
+then
+    echo -e "\e[1;46m Committing changes... \e[0m"
+    git add ./
+    read -r -p "Please enter a commit message: " message
+    git commit -m "$message"
+fi
+echo -e "\e[1;42m Preparing to sync Git repositories \e[0m"
+ssh-add ~/.ssh/afiskforge
+git push origin
+git push 
+echo -e "\e[1;41m You're all set! \e[0m"
